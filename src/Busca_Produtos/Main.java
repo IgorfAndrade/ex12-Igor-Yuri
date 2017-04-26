28b12e10785f576caa08c63088c9130c12684828
11204307 <11204307@W3332046.portoalegre.pucrsnet.br>
2017-04-25 21:38:10 -0300

    penta

--- src/Busca_Produtos/Main.java
+++ src/Busca_Produtos/Main.java
@@ -1,10 +1,12 @@
 package Busca_Produtos;
 
+import java.util.ArrayList;
+
 public class Main {
 
 	public static void main(String[] args) {
 	ProdutoDAO prod = new ProdutoDAO();
-        ArrayList<PrudutoDTO> prodCad = prod.getProdutos();
+        ArrayList<ProdutoDTO> prodCad = (ArrayList<ProdutoDTO>) prod.getProdutos();
 		
 		for (int i = 1; i<= prodCad.size(); i++){
 			
